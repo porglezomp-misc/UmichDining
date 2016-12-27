22501796754a73c2bf09f8253c60f06cb6266486
Caleb Jones <code@calebjones.net>
2016-12-26 23:20:25 -0500

    Initial commit, add API skeleton for DiningHall

---
+++ umich-dining/Menu.swift
@@ -0,0 +1,16 @@
+//
+//  Menu.swift
+//  umich-dining
+//
+//  Created by Caleb Jones on 12/26/16.
+//  Copyright © 2016 Caleb Jones. All rights reserved.
+//
+
+import Foundation
+
+class Menu {
+    // TODO: Support multiple courses?
+    public class Item {
+        // TODO: Traits
+    }
+}
