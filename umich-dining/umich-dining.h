8c1bb51891929ad23b103b4d246f012d3d51f050
Caleb Jones <code@calebjones.net>
2016-12-26 21:59:33 -0500

    Initial Commit

---
+++ umich-dining/umich-dining.h
@@ -0,0 +1,19 @@
+//
+//  umich-dining.h
+//  umich-dining
+//
+//  Created by Caleb Jones on 12/26/16.
+//  Copyright © 2016 Caleb Jones. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for umich-dining.
+FOUNDATION_EXPORT double umich_diningVersionNumber;
+
+//! Project version string for umich-dining.
+FOUNDATION_EXPORT const unsigned char umich_diningVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <umich_dining/PublicHeader.h>
+
+
